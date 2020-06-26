bf7b0a00f541971b793eb288ce540ef3ea0ffd10
Chris Bragg <cbragg9@yahoo.com>
2020-06-25 18:38:07 -0700

    html routes

--- server.js
+++ server.js
@@ -2,18 +2,19 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
+// Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 3000;
 
-const db = require("./models");
-
+// Creating express app and configuring middleware needed for authentication
 const app = express();
-
 app.use(logger("dev"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
+
+// Requiring our routes
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutDB", { useNewUrlParser: true });
 
